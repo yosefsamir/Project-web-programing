d48e7d18c448951bdf27e7398b223ec898ea39e4
yousef samir <yosifsamir454@gmail.com>
2023-11-16 13:24:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/all.css">
+    <link rel="stylesheet" href="/css/fontawesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <div class="content">
+            <div class="draw">
+                <div class="poll"></div>
+                <div class="img_food">
+                    <img src="/img/Food%20image%201.png" alt="food">
+                </div>
+            </div>
+            <div class="top">
+                <div class="logo">
+                        <h1 class="hp1">RES</h1>
+                        <h1 class="hp2">STO</h1>
+                        <div class="line"></div>
+                </div>
+                <ul class="navbar">
+                    <li><a class="f_item"  href="#">Home</a></li>
+                    <li><a href="#">menu</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
+                <div class="icons">
+                    <i class="fa-solid fa-bars"></i>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <!--middle of header-->
+            <div class="middle">
+                <div class="left">
+                    <div class="items">
+                        <h1>Order your</h1>
+                        <h3 class="hp1">Favourite</h3>
+                        <h3 class="hp2">Foods</h3>
+                        <p class="p1">Order your Favourite Foods fresh and tasty dish here will be all the components
+                           of the dish and all details</p>
+                        <p class="total">Total Order : 85.30 <sub class="city">EGP</sub></p>
+
+                        <button class="order-now"><i class=" fa-solid fa-cart-shopping"></i>Order Now</button>
+                    </div>
+                </div>
+                <div class="right">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
